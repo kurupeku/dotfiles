4bf5ae6a721986b1c86ac5f5c439bccde3139e71
pey-chan <yohei.uemura@rakus.co.jp>
2022-05-11 17:01:48 +0900

    #

--- nvim/lua/plugins/nvim-ts-hint-textobject.lua
+++ nvim/lua/plugins/nvim-ts-hint-textobject.lua
@@ -1,2 +1,7 @@
-vim.api.nvim_set_keymap('o', 'm', [[<cmd><C-U>lua require('tsht').nodes()<cr>]], { noremap = false, silent = true })
-vim.api.nvim_set_keymap('v', 'm', [[<cmd>lua require('tsht').nodes()<cr>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  'o', 'm', [[:<C-U>lua require('tsht').nodes()<cr>]], { noremap = false, silent = true }
+)
+
+vim.api.nvim_set_keymap(
+  'v', 'm', [[:lua require('tsht').nodes()<cr>]], { noremap = true, silent = true }
+)

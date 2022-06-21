a2aa53387db73b7b899c4a94e81bff124d62ecbf
pey-chan <yohei.uemura@rakus.co.jp>
2022-06-21 09:25:18 +0900

    refactoring node_setup.sh

--- modules/scripts/node_setup.sh
+++ modules/scripts/node_setup.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 # Node npm packages
-NPM_PACKAGES=(
-  eslint
-  tslint
-  prettier
-  typescript
-  typescript-language-server
-  bash-language-server
-  markdownlint-cli
-  emmet-ls
-  vscode-langservers-extracted
-  @tailwindcss/language-server
-  yaml-language-server
-  graphql-language-service-cli
-  intelephense
-  dockerfile-language-server-nodejs
-  sql-language-server
-  @google/clasp
+NPM_PACKAGES=$(cat << EOF
+eslint
+tslint
+prettier
+typescript
+typescript-language-server
+bash-language-server
+markdownlint-cli
+emmet-ls
+vscode-langservers-extracted
+@tailwindcss/language-server
+yaml-language-server
+graphql-language-service-cli
+intelephense
+dockerfile-language-server-nodejs
+sql-language-server
+@google/clasp
+EOF
 )
 
 echo "update versions what installed npm packages..."
@@ -25,5 +26,5 @@ npm update --location=global npm
 npm update --location=global
 
 echo "install global packages for development"
-npm i --location=global "${NPM_PACKAGES[@]}"
+echo "$NPM_PACKAGES" | xargs -L 1 -P 4 npm i --location=global
 npm audit fix --force

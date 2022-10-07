85beb0576ff1c076dac9e64a3c5f188423261dbc
kurupeku <y.u.kurupeku@gmail.com>
2022-10-07 09:20:55 +0900

    add alias to open file in browser

--- modules/rc/alias.sh
+++ modules/rc/alias.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 alias lg="lazygit"
-alias vi="vim"
 alias vim="nvim"
 alias d="docker"
 alias dc="docker compose"
@@ -10,3 +9,5 @@ alias dct="docker compose -f docker-compose.test.yml"
 alias dcp="docker compose -f docker-compose.prod.yml"
 alias python=python3
 alias pip=pip3
+alias chrome="open -a 'Google Chrome'"
+alias brave="open -a 'Brave'"

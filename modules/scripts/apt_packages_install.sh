f752c88643442c688d164c14b814bad0b4b515eb
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-07-10 16:25:34 +0900

    modify apt packages

--- modules/scripts/apt_packages_install.sh
+++ modules/scripts/apt_packages_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PACKAGES="git curl bash zsh gpg openssl build-essential libyaml-dev \
-          libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev \
-          liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev \
-          zlib1g-dev uuid-dev tk-dev"
+libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev \
+liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev \
+zlib1g-dev uuid-dev tk-dev"
 
 sudo apt-get update
 sudo apt-get install "$PACKAGES"

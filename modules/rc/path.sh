42e675e7ad5789f779418197d3d66f9cd64d88aa
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-07-13 09:19:38 +0900

    add rancher path

--- modules/rc/path.sh
+++ modules/rc/path.sh
@@ -9,3 +9,9 @@ case ":$PATH:" in
 *":$PNPM_HOME:"*) ;;
 *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
+
+# Rancher Desktop の設定
+RD_BIN="$HOME/.rd/bin"
+if [ -d "$RD_BIN" ]; then
+  export PATH="$RD_BIN:$PATH"
+fi

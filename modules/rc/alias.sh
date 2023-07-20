7b13b9a6cffc2d75ba4fa08fde76e121b72cb8ff
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-07-20 10:22:01 +0900

    add kubectl alias

--- modules/rc/alias.sh
+++ modules/rc/alias.sh
@@ -7,6 +7,7 @@ alias dc="docker compose"
 alias dcd="docker compose -f docker-compose.dev.yml"
 alias dct="docker compose -f docker-compose.test.yml"
 alias dcp="docker compose -f docker-compose.prod.yml"
+alias kc="kubectl"
 alias python=python3
 alias pip=pip3
 alias chrome="open -a 'Google Chrome'"

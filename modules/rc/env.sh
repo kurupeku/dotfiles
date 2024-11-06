4e704638496e53b05da3080ab4d8865739fa99dc
kurupeku <22340645+kurupeku@users.noreply.github.com>
2024-11-06 13:29:01 +0900

    feat: remove all env vars

--- modules/rc/env.sh
+++ modules/rc/env.sh
@@ -1,16 +1,1 @@
 #!/bin/sh
-
-export ASDFROOT="$HOME"/.asdf
-export ASDFINSTALLS="$HOME"/.asdf/installs
-
-go_bin_path="$(asdf which go 2>/dev/null)"
-if [ -e "${go_bin_path}" ]; then
-  export GOROOT
-  GOROOT="$(dirname "$(dirname "${go_bin_path}")")"
-
-  export GOPATH
-  GOPATH="$(dirname "${GOROOT}")/packages"
-
-  export GOBIN
-  GOBIN="$(dirname "${GOROOT}")/bin"
-fi
